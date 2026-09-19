c78dce96470bb868b78cb5e658bb6e90bb33099f
cat <hesam123409@gmail.com>
2026-09-20 01:20:02 +0330

    main get changed

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from template import FASTAPI_MAIN
-
-with open('main3.py' , 'w') as f:
-    f.write(FASTAPI_MAIN)
+from ui import ask_name , database
+from fs import create_folder
+name = ask_name()
+print(name)
+try:
+    create_folder(name)
+except Exception as error:
+    print(error)
+db_name = database(['mongoDB' ,'mysql', 'sqlite' , 'postgresql'])
+if db_name == 'sqlite':
+    print('good choice')
+print(db_name)
