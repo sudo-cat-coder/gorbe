dd2b0fed4d9cdfc8f87dd0b8b093e372a126eef1
cat <hesam123409@gmail.com>
2026-09-20 13:45:42 +0330

    ui logic fixed

--- ui.py
+++ ui.py
@@ -2,6 +2,8 @@ import questionary
 from typing import List
 def ask_name():
     name = questionary.text('enter project name').ask()
+    while not name:
+        name = questionary.text('enter project name').ask()
     return name
 
 def database(args : List[str]):
