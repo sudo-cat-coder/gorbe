88856ed15f6e27218373a175a638c925b3f781ad
cat <hesam123409@gmail.com>
2026-09-21 00:48:07 +0330

    if name exits ask again until get unique name

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-from template import FASTAPI_MAIN
-from ui import ask_name , database , pip_or_uv
+from template import template_render
+from ui import ask_name , database , pip_or_uv , docker
 from fs import create_folder
 from commands import inital
 
-
 name = ask_name()
-print(name)
-try:
-    folder = create_folder(name)
-    init = inital(path=folder , name=name)
-
-except Exception as error:
-    print(error)
-
+is_created=create_folder(name=name)
+while not is_created:
+    name = ask_name()
+    is_created = create_folder(name=name)
 db_name = database(['mongoDB' ,'mysql', 'sqlite' , 'postgresql'])
-if db_name == 'sqlite':
-    print('good choice')
 print(db_name)
+
 pov = pip_or_uv()
 print(pov)
+docker = docker() 
+print(docker)
+if docker:
+    template_render({'db_name' : db_name})
