a8d56a933384864de99fb15a0f889346570d5b33
cat <hesam123409@gmail.com>
2026-09-20 13:28:40 +0330

    uv initalized in new folder

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from template import FASTAPI_MAIN
 from ui import ask_name , database
 from fs import create_folder
+from commands import inital
+
+
 name = ask_name()
 print(name)
 try:
-    create_folder(name)
+    folder = create_folder(name)
+    init = inital(path=folder , name=name)
+
 except Exception as error:
     print(error)
+
 db_name = database(['mongoDB' ,'mysql', 'sqlite' , 'postgresql'])
 if db_name == 'sqlite':
     print('good choice')
