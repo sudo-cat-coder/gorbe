ed28483c6fc5b3e22e8e6b7d07fa4ca770453e10
cat <hesam123409@gmail.com>
2026-09-21 20:08:44 +0330

    warning for active venv and fix logic until now

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from template import template_render
-from ui import ask_name , database , pip_or_uv , docker
+from ui import ask_name , database , pip_or_uv , docker , wanrning
 from fs import create_folder
-from commands import inital
+from commands import inital , venv , install_package_pip
 
 name = ask_name()
 is_created=create_folder(name=name)
-while not is_created:
+while not is_created['state']:
     name = ask_name()
     is_created = create_folder(name=name)
+if is_created['state']:
+    print(is_created['path'])
 db_name = database(['mongoDB' ,'mysql', 'sqlite' , 'postgresql'])
 print(db_name)
 
 pov = pip_or_uv()
-print(pov)
+if pov == 'pip':
+    venv(path=is_created['path'])
+    install_package_pip('cowsay' , path=is_created['path'])
 docker = docker() 
 print(docker)
 if docker:
-    template_render({'db_name' : db_name})
+    template_render({'db_name' : db_name} , path= is_created['path'])
+
+wanrning(path=is_created['path'])
