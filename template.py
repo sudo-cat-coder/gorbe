9526b3cdb64d5ea89cc6b6edc805371faf7e4241
cat <hesam123409@gmail.com>
2026-09-21 23:26:46 +0330

    ADD | database template function

--- template.py
+++ template.py
@@ -3,6 +3,8 @@ from typing import Dict
 from jinja2 import Environment , FileSystemLoader
 import os
 
+from jinja2.defaults import TRIM_BLOCKS
+
 def docker_template(content : Dict , path : str):
     env = Environment(loader=FileSystemLoader(os.getcwd()) , trim_blocks=True , lstrip_blocks=True)
     template = env.get_template('templates/docker-compose.yaml.j2')
@@ -13,8 +15,18 @@ def docker_template(content : Dict , path : str):
         return 'docker compose created'
 
 
+def database_template(content : Dict , path:str , pov : str = 'pip' , name:str | None = None ) :
+    env = Environment(loader=FileSystemLoader(os.getcwd()),trim_blocks=True , lstrip_blocks=True)
+    template = env.get_template('/templates/database.py.j2')
+    output = template.render(**content)
+    if pov == 'uv':
+        with open(f'{path}/{name}/database.py') as f:
+            f.write(output)
+            return 'docker created with uv'
 
-
+    with open(f'{path}/database.py') as f:
+        f.write(output)
+        return 'docker created with pip'
 #FASTAPI_MAIN =  "from fastapi import FastAPI\n\napp = FastAPI()\n\n@app.get('/')\nasync def root():\n\treturn {'message' : 'Hello World!'}"
 
 
