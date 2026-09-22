c44245cdc5445d34f978b269563c5ea74e5a71f3
cat <hesam123409@gmail.com>
2026-09-22 13:23:27 +0330

    FIX | fix logic XD

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from template import docker_template
+from template import docker_template ,fastapi_project , database_template
 from ui import ask_name , database , pip_or_uv , docker , wanrning , user_perfrence_for_database , framework
 from fs import create_folder
 from commands import uv_inital , venv , install_package_pip , django_start
@@ -19,21 +19,29 @@ pov = pip_or_uv()
 docker = docker()
 if pov == 'pip':
     venv(path=is_created['path'])
-    install_package_pip('cowsay' , path=is_created['path'])
+    install_package_pip(['cowsay' , 'requests'] , path=is_created['path'])
+    if framework_name == 'Django':
+        django_start(name=name)
+    if framework_name == 'FastAPI':
+        fastapi_project(name=name , pov='pip' ,path=PIP_PATH)
+        database_template(content={'db_name' : db_name} , path=PIP_PATH , name=name , pov='pip')
     if docker :
 
         content = {**user_perfrence_for_database(db_name) , 'db_name' : db_name}
-        docker_template(content , path=PIP_PATH)
+        docker_template(content , path=PIP_PATH ,framework_name=framework_name)
 
 
 if pov == 'uv':
-    uv_inital(path=is_created['path'] , name=name , package=framework_name)
+    uv_inital(path=is_created['path'] , name=name , packages=[framework_name])
     if framework_name == 'Django':
         django_start(name=name)
+    if framework_name == 'FastAPI':
+        fastapi_project(name=name , pov='uv')
+        database_template({'db_name' : db_name},path=UV_PATH , pov='uv',name=name)
     if docker:
 
         content = {**user_perfrence_for_database(db_name) , 'db_name' : db_name}
-        docker_template(content , path=UV_PATH)
+        docker_template(content , path=UV_PATH ,framework_name=framework_name)
 
 
 
